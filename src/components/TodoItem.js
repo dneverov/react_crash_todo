064510fb2e96a183a31787422d2e45ab74a0f0f9
Denis Neverov <denis.neverov@gmail.com>
2020-06-02 04:18:51 +0300

    Add functions to Add, Delete Todo items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      backgroundColor: '#d0e5e0',
+      padding: '10px',
+      borderBottom: '1px dotted #ccc',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+  }
 
   render() {
+    const {id, title} = this.props.todo;
     return (
-      <div>
-        <p>{ this.props.todo.title }</p>
+      <div style={this.getStyle()}>
+        <p>
+          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+          { title }
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+        </p>
       </div>
     );
   }
+}
 
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+};
+
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 9px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
 }
 
 export default TodoItem;
